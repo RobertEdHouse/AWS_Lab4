0b30a7283cdd87783ad9d81a415b5ae339046c64
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-11-23 22:41:55 +0200

    update buildspec.yml

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
  <?php
-    $content = file_get_contents("./testresults/*.trx");
+    $content = file_get_contents("./testresults/test_results.trx");
+    print_r(file_exists("./testresults/test_results.trx"));
     $xmlData = simplexml_load_file($content) 
         or die("Unable to load the report.");
     echo '<pre>';
