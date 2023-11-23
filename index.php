2e7ca2e1d72318882ce23e51f198765212bd4a20
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-11-24 00:12:05 +0200

    update buildspec.yml

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
  <?php
-    $content = readfile("./testresults/test_results.trx")
+    $content = file_get_contents("./testresults/test_results.trx")
         or die("Unable to load the report.");
     echo '<pre>';
+    echo $content;
     print_r("Результат виконання тестів:");
     print_r($content);
     echo '</pre>';
