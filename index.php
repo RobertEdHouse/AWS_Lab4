3a202194f50d40001969a96b8f067f282d74315b
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-11-23 23:40:45 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
  <?php
     $content = file_get_contents("./testresults/test_results.trx");
-    print_r(file_exists("./testresults/test_results.trx"));
+    print_r($content);
     $xmlData = simplexml_load_file($content) 
-        or die($xmlData);
+        or die("Unable to load the report.");
     echo '<pre>';
     print_r($xmlData);
     echo '</pre>';
