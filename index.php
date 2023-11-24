a463f1a5f337a2db877763026859a093120ca58e
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-11-24 09:55:13 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
  <?php
-    $filename = './testresults/test_results.trx';
+    $file_path = './testresults/test_results.trx';
 
-    $content = simplexml_load_file($filename);
-    echo '<pre>';
-    echo $content;
-    echo '</pre>';
+    $xml_content = file_get_contents($file_path);
+
+    $xml = simplexml_load_string($xml_content);
+
+    $result_summary = $xml->xpath('//ResultSummary');
+
+    print_r($result_summary);
 
 ?>
