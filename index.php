1def0f3e1f4d05bfef8c6b09bc237d69dbb908ed
RobertEdHouse <robert.edwin.house101@gmail.com>
2023-11-24 10:05:38 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
  <?php
-    $file_path = './testresults/test_results.trx';
+    $file_path = './testresults/test_results.html';
 
-    $xml_content = file_get_contents($file_path);
-
-    $xml = simplexml_load_string($xml_content);
-
-    $result_summary = $xml->xpath('//ResultSummary');
-
-    print_r($result_summary);
+    $text= file_get_contents($file_path);
+    echo $text;
 
 ?>
